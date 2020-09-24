5dc1ed79404871c73fdb49406e52e589130c1930
marcus-bornman <marcus.bornman@frei.one>
2020-09-24 21:28:44 +0200

    Added model for discharge decision

---
+++ src/main/java/model/DischargeDecision.java
@@ -0,0 +1,9 @@
+package model;
+
+/**
+ * Discharge decision:
+ * I (patient sent to Intensive Care Unit),
+ * S (patient prepared to go home),
+ * A (patient sent to general hospital floor)
+ */
+public enum DischargeDecision {I, S, A}
