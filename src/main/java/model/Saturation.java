b7ee69d8abe3602b5fc82679c47311c857d4baec
marcus-bornman <marcus.bornman@frei.one>
2020-09-24 21:28:04 +0200

    Added model for saturation

---
+++ src/main/java/model/Saturation.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ *      Saturation in %:
+ *      excellent (>= 98), good (>= 90 and < 98),
+ *      fair (>= 80 and < 90), poor (< 80)
+ */
+public enum Saturation {excellent, good, fair, poor}
