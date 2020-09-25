74295ff7cec2620fff994eedcf829cf79ca7399e
marcus-bornman <marcus.bornman@frei.one>
2020-09-25 07:00:15 +0200

    Added PPostOperativeProblem to describe the GP problem

---
+++ src/main/java/gp/problem/PostOperativeProblem.java
@@ -0,0 +1,75 @@
+package gp.problem;
+
+import ec.EvolutionState;
+import ec.Individual;
+import ec.gp.GPIndividual;
+import ec.gp.GPProblem;
+import ec.gp.koza.KozaFitness;
+import ec.simple.SimpleProblemForm;
+import ec.util.Parameter;
+import model.Patient;
+import model.PatientReader;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This class is used for the genetic programming problem that we are solving.
+ */
+public class PostOperativeProblem extends GPProblem implements SimpleProblemForm {
+	private static final long serialVersionUID = 1;
+
+	/**
+	 * The list of patients to use.
+	 */
+	public final List<Patient> patients;
+
+	/**
+	 * The current patient to consider.
+	 */
+	public Patient patient;
+
+	/**
+	 * Reads the training data from training_data.csv to initialise the
+	 * problem instance accordingly.
+	 *
+	 * @throws IOException if the file could not be read successfully.
+	 */
+	public PostOperativeProblem() throws IOException {
+		super();
+		patients = new PatientReader().readFromFile("training_data.csv");
+	}
+
+	@Override
+	public void setup(final EvolutionState state, final Parameter base) {
+		super.setup(state, base);
+
+		if (!(input instanceof Prediction)) {
+			state.output.fatal("GPData class must subclass from " + Prediction.class, base.push(P_DATA), null);
+		}
+	}
+
+	@Override
+	public void evaluate(final EvolutionState state, final Individual ind, final int subpopulation, final int threadnum) {
+		if (ind.evaluated) return;
+
+		Prediction predictionData = (Prediction) (this.input);
+
+		int correctTrainingPredictions = 0;
+		for (Patient patient : patients) {
+			this.patient = patient;
+			((GPIndividual) ind).trees[0].child.eval(state, threadnum, predictionData, stack, ((GPIndividual) ind), this);
+
+			if (predictionData.dischargeDecision == patient.dischargeDecision) correctTrainingPredictions++;
+		}
+
+		((KozaFitness) ind.fitness).setStandardizedFitness(state, ((double) correctTrainingPredictions) / patients.size());
+		((KozaFitness) ind.fitness).hits = correctTrainingPredictions;
+		ind.evaluated = true;
+	}
+}
+
