2467163b2b9a6a64d2fb5219a5f53465990d1d38
marcus-bornman <marcus.bornman@frei.one>
2020-09-25 07:46:14 +0200

    Added internal temperature stability decision node

---
+++ src/main/java/gp/nodes/internal/InternalTemperatureStability.java
@@ -0,0 +1,44 @@
+package gp.nodes.internal;
+
+import ec.EvolutionState;
+import ec.Problem;
+import ec.gp.ADFStack;
+import ec.gp.GPData;
+import ec.gp.GPIndividual;
+import ec.gp.GPNode;
+import gp.problem.PostOperativeProblem;
+
+/**
+ * This class models the internal temperature stability decision node. It accepts 3 children and acts as follows:
+ * - if the patient's internal temperature stability is 'unstable' it evaluates the 1st child.
+ * - if the patient's internal temperature stability is 'moderate' it evaluates the 2nd child.
+ * - if the patient's internal temperature stability is 'stable' it evaluates the 3rd child.
+ */
+public class InternalTemperatureStability extends GPNode {
+	@Override
+	public String toString() {
+		return "ITS";
+	}
+
+	@Override
+	public int expectedChildren() {
+		return 3;
+	}
+
+	@Override
+	public void eval(EvolutionState evolutionState, int i, GPData gpData, ADFStack adfStack, GPIndividual gpIndividual, Problem problem) {
+		PostOperativeProblem postOperativeProblem = ((PostOperativeProblem) (problem));
+
+		switch (postOperativeProblem.patient.internalTemperatureStability) {
+			case unstable:
+				children[0].eval(evolutionState, i, gpData, adfStack, gpIndividual, problem);
+				break;
+			case moderate:
+				children[1].eval(evolutionState, i, gpData, adfStack, gpIndividual, problem);
+				break;
+			case stable:
+				children[2].eval(evolutionState, i, gpData, adfStack, gpIndividual, problem);
+				break;
+		}
+	}
+}
